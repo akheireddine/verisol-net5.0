9b949942ea0c7ce0519ee5120eea1276bdb3e4b3
akheireddine <anissa.kheireddine@dowsers.finance>
2024-09-13 12:26:29 +0200

    Fix all package to be compatible with .NET 5

--- resetInstall.sh
+++ resetInstall.sh
@@ -1,5 +1,5 @@
 dotnet tool uninstall --global VeriSol
 dotnet tool uninstall --global SolToBoogieTest
-dotnet build Sources/VeriSol.sln
-dotnet tool install VeriSol --version 0.1.1-alpha --global --add-source ./nupkg/
-dotnet tool install --global SolToBoogieTest --version 0.1.1-alpha --add-source ./nupkg/
+dotnet build --configuration Release Sources/VeriSol.sln
+dotnet tool install VeriSol --version 0.1.1-alpha --global --add-source ./Sources/nupkg/
+dotnet tool install --global SolToBoogieTest --version 0.1.1-alpha --add-source ./Sources/nupkg/
