5673a6fcee377cdf5e03ca9de78475fcef490073
akheireddine <anissa.kheireddine@dowsers.finance>
2024-10-24 14:21:12 +0200

    Remove some unused stuff in : 	Sources/VeriSol.props

--- resetInstall.sh
+++ resetInstall.sh
@@ -1,5 +1,8 @@
+#! /bin/bash
+DIR=$(dirname $0)
+
 dotnet tool uninstall --global VeriSol
 dotnet tool uninstall --global SolToBoogieTest
-dotnet build --configuration Release Sources/VeriSol.sln
-dotnet tool install VeriSol --version 0.1.1-alpha --global --add-source ./Sources/nupkg/
-dotnet tool install --global SolToBoogieTest --version 0.1.1-alpha --add-source ./Sources/nupkg/
+dotnet build --configuration Release $DIR/Sources/VeriSol.sln
+dotnet tool install VeriSol --version 0.1.1-alpha --global --add-source $DIR/Sources/nupkg/
+dotnet tool install --global SolToBoogieTest --version 0.1.1-alpha --add-source $DIR/Sources/nupkg/
